9391a97b3f9bc6772055121663e61375d10e106a
msarnacki <maciek.sarnacki.konta@gmail.com>
2020-08-19 01:09:35 +0200

    main.py corrected incidents

--- main.py
+++ main.py
@@ -54,18 +54,18 @@ column_names = ['match_id', 'home_team', 'away_team', 'FT_home_sc', 'FT_away_sc'
                 'FT_home_possession','FT_away_possession', 'FT_home_goal_attempts', 'FT_away_goal_attempts', 'FT_home_shots_on_goal', 'FT_away_shots_on_goal',
                 'FT_home_shots_off_goal', 'FT_away_shots_off_goal', 'FT_home_blocked_shots', 'FT_away_blocked_shots', 'FT_home_freekicks', 'FT_away_freekicks',
                 'FT_home_corners', 'FT_away_corners', 'FT_home_offsides', 'FT_away_offsides', 'FT_home_gk_saves', 'FT_away_gk_saves', 'FT_home_fouls', 'FT_away_fouls',
-                'FT_home_yellow_cards', 'FT_away_yellow_cards', 'FT_home_passes', 'FT_away_passes', 'FT_home_passes_completed', 'FT_away_passes_completed',
-                'FT_home_tackles', 'FT_away_tackles', 'FT_home_attacks', 'FT_away_attacks', 'FT_home_dangerous_attacks', 'FT_away_dangerous_attacks',
+                'FT_home_red_cards', 'FT_away_red_cards','FT_home_yellow_cards', 'FT_away_yellow_cards', 'FT_home_passes', 'FT_away_passes', 'FT_home_passes_completed', 
+                'FT_away_passes_completed', 'FT_home_tackles', 'FT_away_tackles', 'FT_home_attacks', 'FT_away_attacks', 'FT_home_dangerous_attacks', 'FT_away_dangerous_attacks',
                 'FH_home_possession','FH_away_possession', 'FH_home_goal_attempts', 'FH_away_goal_attempts', 'FH_home_shots_on_goal', 'FH_away_shots_on_goal',
                 'FH_home_shots_off_goal', 'FH_away_shots_off_goal', 'FH_home_blocked_shots', 'FH_away_blocked_shots', 'FH_home_freekicks', 'FH_away_freekicks',
                 'FH_home_corners', 'FH_away_corners', 'FH_home_offsides', 'FH_away_offsides', 'FH_home_gk_saves', 'FH_away_gk_saves', 'FH_home_fouls', 'FH_away_fouls',
-                'FH_home_yellow_cards', 'FH_away_yellow_cards', 'FH_home_passes', 'FH_away_passes', 'FH_home_passes_completed', 'FH_away_passes_completed',
-                'FH_home_tackles', 'FH_away_tackles', 'FH_home_attacks', 'FH_away_attacks', 'FH_home_dangerous_attacks', 'FH_away_dangerous_attacks',
+                'FH_home_red_cards', 'FH_away_red_cards', 'FH_home_yellow_cards', 'FH_away_yellow_cards', 'FH_home_passes', 'FH_away_passes', 'FH_home_passes_completed', 
+                'FH_away_passes_completed', 'FH_home_tackles', 'FH_away_tackles', 'FH_home_attacks', 'FH_away_attacks', 'FH_home_dangerous_attacks', 'FH_away_dangerous_attacks',
                 'SH_home_possession','SH_away_possession', 'SH_home_goal_attempts', 'SH_away_goal_attempts', 'SH_home_shots_on_goal', 'SH_away_shots_on_goal',
                 'SH_home_shots_off_goal', 'SH_away_shots_off_goal', 'SH_home_blocked_shots', 'SH_away_blocked_shots', 'SH_home_freekicks', 'SH_away_freekicks',
                 'SH_home_corners', 'SH_away_corners', 'SH_home_offsides', 'SH_away_offsides', 'SH_home_gk_saves', 'SH_away_gk_saves', 'SH_home_fouls', 'SH_away_fouls',
-                'SH_home_yellow_cards', 'SH_away_yellow_cards', 'SH_home_passes', 'SH_away_passes', 'SH_home_passes_completed', 'SH_away_passes_completed',
-                'SH_home_tackles', 'SH_away_tackles', 'SH_home_attacks', 'SH_away_attacks', 'SH_home_dangerous_attacks', 'SH_away_dangerous_attacks',
+                'SH_home_red_cards', 'SH_away_red_cards', 'SH_home_yellow_cards', 'SH_away_yellow_cards', 'SH_home_passes', 'SH_away_passes', 'SH_home_passes_completed',
+                'SH_away_passes_completed', 'SH_home_tackles', 'SH_away_tackles', 'SH_home_attacks', 'SH_away_attacks', 'SH_home_dangerous_attacks', 'SH_away_dangerous_attacks',
                 'home_player_1', 'home_player_2', 'home_player_3', 'home_player_4', 'home_player_5', 'home_player_6', 'home_player_7', 'home_player_8', 'home_player_9', 'home_player_10',  'home_player_11',
                 'home_sub_1', 'home_sub_2', 'home_sub_3', 'home_sub_4', 'home_sub_5', 'home_sub_6', 'home_sub_7', 'home_sub_8', 'home_sub_9',
                 'away_player_1', 'away_player_2', 'away_player_3', 'away_player_4', 'away_player_5', 'away_player_6', 'away_player_7', 'away_player_8', 'away_player_9', 'away_player_10',  'away_player_11',
@@ -101,7 +101,7 @@ print(len(ids))
 
 matches = []
 
-for id in ids[:5]:  
+for id in ids[:30]:  
 #for id in ids:    
     match = []
     match.append(id)
@@ -242,6 +242,8 @@ for id in ids[:5]:
                 elif 'incidentRow--home' in inc.findParent(recursive = False).get('class'):
                     team = "H"   
             #type of incidents
+            elif 'soccer-ball-own' in inc.get('class'):
+                incident_name = "O-"
             elif 'soccer-ball' in inc.get('class'):
                 incident_name = "G-"
             elif 'y-card' in inc.get('class'):
@@ -250,6 +252,10 @@ for id in ids[:5]:
                 incident_name = "R-"
             elif 'substitution-in' in inc.get('class'):
                 incident_name = "S-"
+            elif 'penalty-missed' in inc.get('class'):
+                incident_name = "P-"
+            elif 'var' in inc.get('class'):
+                incident_name = "V-"
             #main person
             elif any(string in ['substitution-in-name','participant-name'] for string in inc.get('class')):
                 who = str(inc.text)
@@ -280,9 +286,6 @@ df = pd.DataFrame(matches, columns = column_names)
 print(df.head())
 
 df.to_excel("Premier_League_19_20.xlsx")
-
-#### wziąć pod uwagę var w wydarzeniach
-
 
 
 
